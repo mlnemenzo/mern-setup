c832af2fd75dc3d0cc60fd51fb597c2f543d0286
marknemenzo <mlnemenzo@gmail.com>
2018-05-10 17:45:05 -0700

    mern setup complete

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -5,10 +5,29 @@ class Test extends Component {
 
     componentDidMount() {
         this.getUserData();
+        this.getArticle();
+        this.sendData();
+    }
+
+    async sendData() {
+        const dataToSend = {
+            something: 'here is something',
+            anotherThing: 'here is this',
+            stuff: 'i like stuff'
+        };
+
+        const response = await axios.post('/api/send-data', dataToSend);
+
+        console.log('Send Data: ', response);
+    }
+
+    async getArticle() {
+        const response = await axios.get('/api/get-article');
+        console.log('Article Data: ', response);
     }
 
     async getUserData() {
-        const response = await axios.get('http://localhost:9000/user-data');
+        const response = await axios.get('/api/user-data');
 
         console.log('User Data Response: ', response);
     }
--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express');
+const cors = require('cors');
 const PORT = process.env.PORT || 9000;
-
+const { resolve } = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>App is working!</h1>');
+app.use(cors());
+app.use(express.json());
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
+
+app.post('/api/send-data', (req, res) => {
+    console.log('Data sent: ', req.body);
+
+    res.send({
+        success: true, mirror: req.body
+    });
 });
+
+app.get('/api/user-data', (req, res) => {
+    const user = {
+        name: 'Jim Bob',
+        email: 'jimthebob@mail.com',
+    }
+
+    res.send(user);
+});
+
+app.get('/api/get-article', (req,res) => {
+    const article = {
+        title: 'Some awesome article',
+        author: "Jonnie Walker",
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolorum minus odio temporibus eius? Voluptatem autem perferendis praesentium magni earum.'
+
+    }
+    res.send(article);
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(resolve(__dirname, 'client', 'dist', 'index.html'));
+}) 
 
 app.listen(PORT, () => {
     console.log('Server running at localhost: ' + PORT);
